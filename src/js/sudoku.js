766d96d004a9c0c4c8df072045e410074e8892df
Tejen Shrestha <shrestha.tejen@gmail.com>
2017-10-30 18:13:40 -0400

    app update

--- src/js/sudoku.js
+++ src/js/sudoku.js
@@ -5,14 +5,41 @@ export default class Sudoku {
   constructor() {
     this.game = generator.getGame();
     this.puzzle = this.game.puzzle;
+    this.original = new Map(this.puzzle);
     this.solution = this.game.solution;
 
     this.grid = new SudokuGrid();
     this.grid.fillClues(this.puzzle);
-    this.setInputEvents();
+
+    this.setKeyInputEvents();
+    this.setBtnInputEvents();
+
+    document.getElementById('reset').onclick = () => this.reset();
+    document.getElementById('erase').onclick = () => this.erase();
   }
 
-  setInputEvents() {
+  reset() {
+    this.puzzle = new Map(this.original);
+    this.grid.fillClues(this.puzzle);
+  }
+
+  erase() {
+    this.grid.setSquareValue('', this.puzzle);
+  }
+
+  setBtnInputEvents() {
+    let grid = this.grid;
+    let buttons = document.querySelectorAll('.number-btn');
+
+    buttons.forEach(b => {
+      b.onclick = () => {
+        grid.setSquareValue(b.value, this.puzzle);
+        if (this.solved()) Sudoku.displayMessage();
+      }
+    });
+  }
+
+  setKeyInputEvents() {
     let grid = this.grid;
     let squares = this.grid.squares;
 
