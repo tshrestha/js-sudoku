52deacf6ee0af2cb7acb685bd10560d764e106b2
Tejen Shrestha <shrestha.tejen@gmail.com>
2018-03-31 10:59:46 -0400

    app update

--- webpack.config.js
+++ webpack.config.js
@@ -4,37 +4,41 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/js/sudoku-ui.js'),
-  output: {
-    path: path.resolve(__dirname, 'app'),
-    filename: 'main.[hash].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules(?!\/webpack-dev-server)/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['app']),
-    new ExtractTextPlugin('main.[hash].css'),
-    new webpack.optimize.UglifyJsPlugin({sourceMap: true}),
-    new HtmlWebpackPlugin({template: 'src/index.html'})
-  ],
-  devServer: {
-    port: 8000,
-    contentBase: path.join(__dirname, 'app')
-  },
-  devtool: 'inline-source-map'
-};
+module.exports = options => {
+  const env = process.env.NODE_ENV;
+  
+  return {
+    entry: path.resolve(__dirname, 'src/js/sudoku-ui.js'),
+    output: {
+      path: path.resolve(__dirname, 'app'),
+      filename: 'main.[hash].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules(?!\/webpack-dev-server)/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader'
+          })
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(['app']),
+      new ExtractTextPlugin('main.[hash].css'),
+      new webpack.optimize.UglifyJsPlugin({sourceMap: env !== 'production'}),
+      new HtmlWebpackPlugin({ template: 'src/index.html' })
+    ],
+    devServer: {
+      port: 8000,
+      contentBase: path.join(__dirname, 'app')
+    },
+    devtool: 'inline-source-map'
+  }
+}
